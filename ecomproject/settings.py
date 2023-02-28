03efda6043cb60620557f2a382fce6fb2f46970f
yashanki kotnala <yashankikotnala64@gmail.com>
2023-02-28 12:52:34 +0530

    settings urls

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = "static/"
-STATICFILES_DIRS =os.path.join(BASE_DIR, "static")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", 'static')
+#STATICFILES_DIRS =os.path.join(BASE_DIR, "static")
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", 'static')
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR /"media"
--- main/models.py
+++ main/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from django.contrib.auth.models import AbstractUser
+from .manager import UserManager
 
 # Create your models here.
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=12, unique=True)
+    is_phone_verified = models.BooleanField(default=False)
+    otp = models.CharField(max_length=6)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+    OBJECTS = UserManager()
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
